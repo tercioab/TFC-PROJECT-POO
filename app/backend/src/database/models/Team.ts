57171d59781376e04797be0d863e533257201a3b
walthercio Almeida <suaagenciainicial@gmail.com>
2023-01-23 17:34:34 -0300

    feat: user model

---
+++ app/backend/src/database/models/Team.ts
@@ -0,0 +1,43 @@
+import { INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  id!: number;
+  username!: string;
+  role!: string;
+  email!: string;
+  password!: string;
+}
+
+User.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  // ... Outras configs
+  underscored: true,
+  sequelize: db,
+  modelName: 'Users',
+  timestamps: false,
+});
+
+export default User;
