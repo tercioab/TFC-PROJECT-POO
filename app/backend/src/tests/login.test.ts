030ea6b77dcae12ce0c90ba8a8f7f3a5fcd03e04
walthercio Almeida <suaagenciainicial@gmail.com>
2023-01-24 16:30:51 -0300

    test: verify email and password

---
+++ app/backend/src/tests/login.test.ts
@@ -0,0 +1,35 @@
+
+
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import usersModel from '../database/models/User.model';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+const validUser = {
+    username: 'User',
+    role: 'user',
+    email: 'user@user.com',
+    password: '$2a$08$Y8Abi8jXvsXyqm.rmp0B.uQBA5qUz7T6Ghlg/CvVr/gLxYj5UAZVO', 
+}
+
+describe('Testes login', () => {
+    afterEach(sinon.restore)
+    it('Verifica se o email foi informado', async () => {
+        const result = await chai.request(app).post('/login').send({ password: validUser.password })
+        expect(result.status).to.be.equal(400);
+        expect(result.body).to.be.deep.equal({message: 'All fields must be filled'})
+    })
+
+    it('Verifica se a senha foi informada', async () => {
+        const result = await chai.request(app).post('/login').send({ password: validUser.email })
+        expect(result.status).to.be.equal(400);
+        expect(result.body).to.be.deep.equal({message: 'All fields must be filled'})
+    })
+})
