99a11abf235f60030cca8d8c55791da312fab45e
walthercio Almeida <suaagenciainicial@gmail.com>
2023-01-25 18:22:07 -0300

    refactor: withdraw coments in controller of user

--- app/backend/src/controller/user.controller.ts
+++ app/backend/src/controller/user.controller.ts
@@ -1,21 +1,6 @@
 import { Request, Response } from 'express';
 import UserService from '../services/user.service';
 
-// import { Request, Response } from 'express';
-// import UserService from '../services/user.service';
-
-// const findUser = async (req: Request, res: Response) => {
-//   const service = new UserService();
-//   const { email, password } = req.body;
-//   const { status, message } = await service.findUser(email, password);
-//   if (status === 200) {
-//     res.status(status).json({ token: message });
-//   } else {
-//     res.status(status).json({ message });
-//   }
-// };
-// export default { findUser };
-
 class UserController {
   private _userService: UserService;
 
