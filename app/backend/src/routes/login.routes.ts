0b301d471e8a67d7c1c677d78ee8743816cf4a40
walthercio Almeida <suaagenciainicial@gmail.com>
2023-01-25 00:39:58 -0300

    feat: route to make login

---
+++ app/backend/src/routes/login.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import validateLogin from '../middlewares/user.middlewares';
+
+import UsersController from '../controller/user.controller';
+
+const routers = Router();
+
+const user = new UsersController();
+
+routers.post('/', validateLogin, user.findUser);
+
+export default routers;
