4e89f71093c9df52c257101eacdbd57033dbf33d
walthercio Almeida <suaagenciainicial@gmail.com>
2023-01-25 00:39:23 -0300

    feat: middlewares to validade email and login

---
+++ app/backend/src/middlewares/user.middlewares.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from 'express';
+import * as Joi from '@hapi/joi';
+
+export default function validateLogin(req: Request, res: Response, next: NextFunction) {
+  const { email, password } = req.body;
+  const emailValidation = Joi.string().email().validate(email);
+  const passwordValidation = Joi.string().min(6).validate(password);
+  if (emailValidation.error || passwordValidation.error) {
+    res.status(401).json({ message: 'Incorrect email or password' });
+  } else {
+    next();
+  }
+}
