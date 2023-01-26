fe5edf2b3561f40ed1cf77e9d9f5e1e672a2139c
walthercio Almeida <suaagenciainicial@gmail.com>
2023-01-25 23:23:26 -0300

    fix linter

--- app/backend/src/middlewares/user.middlewares.ts
+++ app/backend/src/middlewares/user.middlewares.ts
@@ -1,17 +1,21 @@
-import { Request, Response, NextFunction } from 'express';
-import * as Joi from 'joi';
+import { Request, Response, NextFunction } from "express";
+import * as Joi from "joi";
 
-export default function validateLogin(req: Request, res: Response, next: NextFunction) {
+export default function validateLogin(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
   const { email, password } = req.body;
 
   const emailValidation = Joi.string().email().validate(email);
   const passwordValidation = Joi.string().min(6).validate(password);
 
   if (!email || !password) {
-    return res.status(400).json({ message: 'All fields must be filled' });
+    return res.status(400).json({ message: "All fields must be filled" });
   }
   if (emailValidation.error || passwordValidation.error) {
-    return res.status(401).json({ message: 'Incorrect email or password' });
+    return res.status(401).json({ message: "Incorrect email or password" });
   }
   next();
 }
