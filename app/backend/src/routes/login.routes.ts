5514ea10eb3b5eaa8cb80da264eb41a70414eedf
walthercio Almeida <suaagenciainicial@gmail.com>
2023-01-25 23:34:44 -0300

    Lint

--- app/backend/src/routes/login.routes.ts
+++ app/backend/src/routes/login.routes.ts
@@ -1,20 +1,20 @@
-import { Router } from "express";
-import validateLogin from "../middlewares/user.middlewares";
-import JWT from "../auth/jwtFunctions";
+import { Router } from 'express';
+import validateLogin from '../middlewares/user.middlewares';
+import JWT from '../auth/jwtFunctions';
 
-import UserController from "../controller/user.controller";
+import UserController from '../controller/user.controller';
 
-const routers = Router();
+const routers = Router(); 
 
 const user = new UserController();
 const jwt = new JWT();
 
 routers.get(
-  "/validate",
-  (req, res, next) => jwt.verifyToken(req, res, next),
-  (req, res) => user.userRole(req, res)
+  '/validate', (req, res, next) => jwt
+    .verifyToken(req, res, next), (req, res) => user
+    .userRole(req, res)
 );
 
-routers.post("/", validateLogin, (req, res) => user.findUser(req, res));
+routers.post('/', validateLogin, (req, res) => user.findUser(req, res));
 
-export default routers;
+export default routers;  
