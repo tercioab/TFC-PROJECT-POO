be9f7a04c1a31b0696a3d59fbfa7a21a251f1554
walthercio Almeida <suaagenciainicial@gmail.com>
2023-01-28 01:19:14 -0300

    feat: service of matches to get matches in progress

--- app/backend/src/services/match.service.ts
+++ app/backend/src/services/match.service.ts
@@ -17,4 +17,15 @@ export default class MatchService {
     });
     return matches;
   }
+
+  public async progressMatch(inProgress: boolean) {
+    const matches = await this._matchModel.findAll({
+      where: { inProgress },
+      include: [
+        { model: teamModel, as: 'homeTeam', attributes: ['teamName'] },
+        { model: teamModel, as: 'awayTeam', attributes: ['teamName'] },
+      ],
+    });
+    return matches;
+  }
 }
