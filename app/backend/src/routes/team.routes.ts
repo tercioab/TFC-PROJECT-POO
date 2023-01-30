086a7c492152e4eb9113a477e266d812e09fb8b8
walthercio Almeida <suaagenciainicial@gmail.com>
2023-01-30 03:11:23 -0300

    feat: add new route to test

--- app/backend/src/routes/team.routes.ts
+++ app/backend/src/routes/team.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import TeamController from '../controller/team.controller';
+import LeaderBoardController from '../controller/leaderboard.controller';
 
 const routers = Router();
 
 const team = new TeamController();
+const leader = new LeaderBoardController();
 
+routers.get('/teste', (req, res) => leader.tableTest(req, res));
 routers.get('/', (req, res) => team.all(req, res));
 routers.get('/:id', (req, res) => team.byId(req, res));
 
