5d7037abb98bedaba49fd2219223a4fc9deacd93
walthercio Almeida <suaagenciainicial@gmail.com>
2023-01-30 03:10:00 -0300

    refactor: coment calculate points to test service

--- app/backend/src/controller/leaderboard.controller.ts
+++ app/backend/src/controller/leaderboard.controller.ts
@@ -1,24 +1,31 @@
-// import { Request, Response } from 'express';
+import { Request, Response } from 'express';
 // import Team from '../database/models/Team.model';
-import IMatch from '../interface/IMatch';
+// import IMatch from '../interface/IMatch';
+import LeaderboardService from '../services/leaderBoard.service';
 
 export default class LeaderBoardController {
-  private _matches: IMatch[];
+//   private _matches: IMatch[];
+  private _leaderService;
 
-  constructor(matches:IMatch[]) {
-    this._matches = matches;
+  constructor() {
+    this._leaderService = new LeaderboardService();
   }
 
-  public async calculatePoints() {
-    let totalPoints = 0;
-    this._matches.forEach((match) => {
-      if (match.homeTeamGoals > match.awayTeamGoals) {
-        totalPoints += 3;
-      }
-      if (match.homeTeamGoals === match.awayTeamGoals) {
-        totalPoints += 1;
-      }
-    });
-    return totalPoints;
+  //   public async calculatePoints() {
+  //     let totalPoints = 0;
+  //     this._matches.forEach((match) => {
+  //       if (match.homeTeamGoals > match.awayTeamGoals) {
+  //         totalPoints += 3;
+  //       }
+  //       if (match.homeTeamGoals === match.awayTeamGoals) {
+  //         totalPoints += 1;
+  //       }
+  //     });
+  //     return totalPoints;
+  //   }
+
+  public async tableTest(req: Request, res: Response) {
+    const result = this._leaderService.getAllMatches();
+    return res.status(200).json(result);
   }
 }
