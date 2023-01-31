a18f890b59025f08978aadd4d8641a954cce6b54
walthercio Almeida <suaagenciainicial@gmail.com>
2023-01-30 21:04:20 -0300

    feat: route to get all leaderboards

--- app/backend/src/controller/leaderboard.controller.ts
+++ app/backend/src/controller/leaderboard.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import LeaderboardService from '../services/leaderBoard.service';
+import ITable from '../interface/ITable';
 
 export default class LeaderBoardController {
   private _leaderService;
@@ -16,5 +17,41 @@ export default class LeaderBoardController {
   public async leaderBoardHome(req: Request, res: Response) {
     const result = await this._leaderService.getAllHomeMatches();
     return res.status(200).json(result);
+  }
+
+  public sumMatches = (a: ITable, b: ITable | undefined) => {
+    if (!b) {
+      return a;
+    }
+
+    return {
+
+      name: a.name,
+      totalPoints: a.totalPoints + b.totalPoints,
+      totalGames: a.totalGames + b.totalGames,
+      totalVictories: a.totalVictories + b.totalVictories,
+      totalDraws: a.totalDraws + b.totalDraws,
+      totalLosses: a.totalLosses + b.totalLosses,
+      goalsFavor: a.goalsFavor + b.goalsFavor,
+      goalsOwn: a.goalsOwn + b.goalsOwn,
+      goalsBalance: a.goalsBalance + b.goalsBalance,
+      efficiency: (Number([(a.totalPoints + b.totalPoints)
+        / ((a.totalGames + b.totalGames) * 3)]) * 100).toFixed(2),
+
+    };
+  };
+
+  public async teste(req: Request, res: Response) {
+    const homeMatches = await this._leaderService.getAllHomeMatches();
+    const awayMatches = await this._leaderService.getAllAwayMatches();
+    const sumMatches = homeMatches.map((homeMatch) => this
+      .sumMatches(homeMatch, awayMatches.find((m) => m.name === homeMatch.name)))
+      .sort((a, b) => b.totalPoints - a.totalPoints
+      || b.totalVictories - a.totalVictories
+      || b.goalsBalance - a.goalsBalance
+      || b.goalsFavor - a.goalsFavor
+        || b.goalsOwn - a.goalsOwn);
+
+    return res.status(200).json(sumMatches);
   }
 }
--- app/backend/src/routes/leaderBoard.routes.ts
+++ app/backend/src/routes/leaderBoard.routes.ts
@@ -6,6 +6,7 @@ const routers = Router();
 
 const leader = new LeaderBoardController();
 
+routers.get('/', (req, res) => leader.teste(req, res));
 routers.get('/home', (req, res) => leader.leaderBoardHome(req, res));
 routers.get('/away', (req, res) => leader.leaderBoardAway(req, res));
 
