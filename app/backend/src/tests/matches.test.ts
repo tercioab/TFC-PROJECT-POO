6e638def67cb263ba52af0d03f7cc71d7ea0d5a4
walthercio Almeida <suaagenciainicial@gmail.com>
2023-02-01 03:55:33 -0300

    test: for matches

--- app/backend/src/tests/matches.test.ts
+++ app/backend/src/tests/matches.test.ts
@@ -10,13 +10,14 @@ import Matches from '../database/models/Match.model';
 import { Response } from 'superagent';
 import { response } from 'express';
 
-import { allMatches, oneMatch } from './mock/Matches.mock';
+import { allMatches} from './mock/Matches.mock';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
 
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwidXNlcm5hbWUiOiJBZG1pbiIsInJvbGUiOiJhZG1pbiIsImVtYWlsIjoiYWRtaW5AYWRtaW4uY29tIiwicGFzc3dvcmQiOiIkMmEkMDgkeGkuSHhrMWN6QU8wblpSLi5CMzkzdTEwYUVEMFJRMU4zUEFFWFE3SHh0TGpLUEVaQnUuUFciLCJpYXQiOjE2NzUyMzQyNDMsImV4cCI6MTY3NTI3MDI0M30.st0L1sei6ZM9EJ5sijZ8dWX26h4HprO2-YxnYohVhhI"
 
 describe('Matches', () => {
   afterEach(sinon.restore);
@@ -29,22 +30,16 @@ describe('Matches', () => {
 
   });
 
-  it('2 se inserir o id ', function (done) {
-    this.timeout(20000);
-    sinon.stub(Matches, 'findOne').resolves(oneMatch as any);
-    chai
+  it('2 Verifica que a partida é finalizada com sucesso ', async () => {
+    sinon.stub(Matches, 'update').resolves([1] as any);
+    const result = await chai
       .request(app)
-      .patch('/matches/1/finish')
-      .then((result) => {
-        expect(result.status).to.equal(200);
-        expect(result.body).to.deep.equal({
-          "message": "Finished"
-        });
-        done();
-      })
-      .catch((err) => {
-        done(err);
-      });
+        .patch('/matches/1/finish')
+          .set('Authorization', token)      
+          expect(result.status).to.be.equal(200);
+    expect(result.body).to.deep.equal({
+      "message": "Finished"
+    });
   });
   
     
--- app/backend/src/tests/mock/Matches.mock.ts
+++ app/backend/src/tests/mock/Matches.mock.ts
@@ -27,18 +27,3 @@ export const allMatches = [
         "teamName": "Santos"
       }
     }]
-
-    export const oneMatch = {
-        "id": 1,
-        "homeTeamId": 16,
-        "homeTeamGoals": 1,
-        "awayTeamId": 8,
-        "awayTeamGoals": 1,
-        "inProgress": false,
-        "homeTeam": {
-          "teamName": "São Paulo"
-        },
-        "awayTeam": {
-          "teamName": "Grêmio"
-        }
-      }
